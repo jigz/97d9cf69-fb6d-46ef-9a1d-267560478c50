ff95543f7eebb23564809d866c84e592439bda32
Jigz <jigzpalillo@gmail.com>
2025-01-10 18:01:51 +0800

    Fix tailwind purge

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import { Card } from '@repo/ui/card'
+import { TrackingsTable } from '@/components/TrackingsTable'
 
 export default function Page(): JSX.Element {
   return (
     <main className='flex flex-col items-center justify-between min-h-screen p-24'>
-      
+      <TrackingsTable />
     </main>
   )
 }
